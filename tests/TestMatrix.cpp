52f4304d43109ff85ae742959db39251ef015981
unknown <ATarasenko@ATarasenko.RStyle>
2014-03-11 12:59:00 +0200

    added test case for operator*() with only one value (it's name is ValueIs2 at MatrixMultiplyWithOneValue test)

--- tests/TestMatrix.cpp
+++ tests/TestMatrix.cpp
@@ -186,3 +186,18 @@ TEST(MatrixConstructor, ConstructorFromVectorWithMatrix0x0)
     EXPECT_EQ(0, matrix.getRowsNumber());
     EXPECT_EQ(0, matrix.getColumnsNumber());
 }
+
+TEST(MatrixMultiplyWithOneValue, ValueIs2)
+{
+    std::vector<int> values;
+    values.push_back(1);
+    values.push_back(2);
+    values.push_back(3);
+    values.push_back(4);
+    Matrix<int> matrix(2, 2, values);
+    matrix = matrix * 2;
+    EXPECT_EQ(2, matrix.getElement(0, 0));
+    EXPECT_EQ(4, matrix.getElement(0, 1));
+    EXPECT_EQ(6, matrix.getElement(1, 0));
+    EXPECT_EQ(8, matrix.getElement(1, 1));
+}
