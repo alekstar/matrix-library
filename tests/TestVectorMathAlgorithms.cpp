ed8bc31e94dc7d8749a910b73f61cec9432a815d
unknown <ATarasenko@ATarasenko.RStyle>
2014-03-13 17:54:59 +0200

    changed tests because of changes in makeScalarMultiplication() function because now it calculate scalar multiplication which is the one number (if CUSTOM_TYPE = int)

--- tests/TestVectorMathAlgorithms.cpp
+++ tests/TestVectorMathAlgorithms.cpp
@@ -9,22 +9,16 @@ TEST(TestMakeScalarMultiplication, TwoVectorsWithTwoElements)
     std::vector<int> vector2;
     vector2.push_back(3);
     vector2.push_back(4);
-    std::vector<int> result = 
-        vectorMathAlogirthms::makeScalarMultiplication(vector1,
-                                                       vector2);
-    EXPECT_EQ(2, result.size());
-    EXPECT_EQ(3, result.at(0));
-    EXPECT_EQ(8, result.at(1));
+    EXPECT_EQ(11, vectorMathAlogirthms::makeScalarMultiplication(vector1,
+                                                                vector2));
 }
 
 TEST(TestMakeScalarMultiplication, TwoVectorsWithNoneElements)
 {
     std::vector<int> vector1;
     std::vector<int> vector2;
-    std::vector<int> result = 
-        vectorMathAlogirthms::makeScalarMultiplication(vector1,
-                                                       vector2);
-    EXPECT_EQ(0, result.size());
+    EXPECT_EQ(0, vectorMathAlogirthms::makeScalarMultiplication(vector1,
+                                                                vector2));
 }
 
 TEST(TestMakeScalarMultiplication, TwoVectorsWithDifferentNumberOfElements)
@@ -33,9 +27,7 @@ TEST(TestMakeScalarMultiplication, TwoVectorsWithDifferentNumberOfElements)
     vector1.push_back(1);
     std::vector<int> vector2;
     vector2.push_back(3);
-    vector2.push_back(4);
-    std::vector<int> result = 
-        vectorMathAlogirthms::makeScalarMultiplication(vector1,
-                                                       vector2);
-    EXPECT_EQ(0, result.size());
+    vector2.push_back(4);        
+    EXPECT_EQ(0, vectorMathAlogirthms::makeScalarMultiplication(vector1,
+                                                                vector2));
 }
