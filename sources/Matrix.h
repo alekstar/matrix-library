7d5fae4aa1271683308616a7a8cc0cdf2f776ec2
unknown <ATarasenko@ATarasenko.RStyle>
2014-03-14 16:09:45 +0200

    added definition for operator+()

--- sources/Matrix.h
+++ sources/Matrix.h
@@ -180,7 +180,31 @@ public:
         }
         return result;
     }
-    Matrix<CUSTOM_TYPE> operator+(const Matrix<CUSTOM_TYPE> &operand);
+    
+    Matrix<CUSTOM_TYPE> operator+(const Matrix<CUSTOM_TYPE> &operand)
+    {
+        if(getRowsNumber()      != operand.getRowsNumber()      || 
+           getColumnsNumber()   != operand.getColumnsNumber()   )
+        {
+            return Matrix<CUSTOM_TYPE>();
+        }
+        Matrix<CUSTOM_TYPE> result(getRowsNumber(), getColumnsNumber());
+        for(natural row_index = 0; row_index < getRowsNumber(); ++row_index)
+        {
+            for(natural column_index = 0; 
+                column_index < getColumnsNumber(); 
+                ++column_index)
+            {
+                result.setElement(getElement(row_index, column_index) + 
+                                  operand.getElement(row_index, column_index),
+                                  row_index,
+                                  column_index);
+            }
+        }
+        
+        return result;
+    }
+    
     Matrix<CUSTOM_TYPE> operator-(const Matrix<CUSTOM_TYPE> &operand);
     bool operator==(const Matrix<CUSTOM_TYPE> &operand);
 private:
