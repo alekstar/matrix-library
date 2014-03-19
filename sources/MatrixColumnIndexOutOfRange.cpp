d82ffa9c6b2c98c763ac33fd8a758c284686cfda
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2014-03-19 10:23:28 +0200

    removed trash

--- sources/MatrixColumnIndexOutOfRange.cpp
+++ sources/MatrixColumnIndexOutOfRange.cpp
@@ -1,10 +1,3 @@
-/* 
- * File:   MatrixColumnIndexOutOfRange.cpp
- * Author: ATarasenko
- * 
- * Created on March 18, 2014, 2:15 PM
- */
-
 #include "MatrixColumnIndexOutOfRange.h"
 
 MatrixColumnIndexOutOfRange::MatrixColumnIndexOutOfRange()
