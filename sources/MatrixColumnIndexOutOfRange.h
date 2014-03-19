a6c509c7b920964daad43057aff7345271427ee7
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2014-03-19 10:23:54 +0200

    removed include natural_type.h

--- sources/MatrixColumnIndexOutOfRange.h
+++ sources/MatrixColumnIndexOutOfRange.h
@@ -1,7 +1,6 @@
 #ifndef MATRIXCOLUMNINDEXOUTOFRANGE_H
 #define	MATRIXCOLUMNINDEXOUTOFRANGE_H
 #include <stdexcept>
-#include "natural_type.h"
 
 class MatrixColumnIndexOutOfRange: public std::out_of_range {
 public:
