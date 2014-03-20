5bce4f29d12181282ce1817faca716810d1fb3c8
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2014-03-20 14:43:11 +0200

    fixed case SizesAreNotEqual at test MatrixOperatorMinus because of adding throw of exception MatricesCantBeAddedOrSubstracted to method operator-(with Matrix)

--- tests/TestMatrix.cpp
+++ tests/TestMatrix.cpp
@@ -628,8 +628,7 @@ TEST(MatrixOperatorMinus, SizesAreNotEqual)
 {
     Matrix<int> matrix2x3(2, 3);
     Matrix<int> matrix3x3(3, 3);
-    Matrix<int> result = matrix2x3 - matrix3x3;
-    EXPECT_TRUE(result.isNull());
+    EXPECT_THROW(matrix2x3 + matrix3x3, MatricesCantBeAddedOrSubstracted);
 }
 
 TEST(MatrixOperatorIsEqual, AllCases)
