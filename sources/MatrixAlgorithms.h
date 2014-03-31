b94806daf2d71f79c400d003a10f818e3b176a43
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2014-03-31 16:28:08 +0300

    removed functions getRowVector() and getColumnVector()

--- sources/MatrixAlgorithms.h
+++ sources/MatrixAlgorithms.h
@@ -11,20 +11,6 @@ namespace MatrixAlgorithms
     using namespace MatrixExceptions;
     
     template <typename ELEMENT_TYPE>
-    vector<ELEMENT_TYPE> getRowVector(const Matrix<ELEMENT_TYPE> &matrix, 
-                                      const natural row_number)
-    {
-        return matrix.getRowVector(row_number);
-    }
-
-    template <typename ELEMENT_TYPE>
-    vector<ELEMENT_TYPE> getColumnVector(const Matrix<ELEMENT_TYPE> &matrix, 
-                                         const natural column_number)
-    {
-        return matrix.getColumnVector(column_number);
-    }
-    
-    template <typename ELEMENT_TYPE>
     bool haveZeroRows(const Matrix<ELEMENT_TYPE> &matrix)
     {
         for(natural row_index = 0;
