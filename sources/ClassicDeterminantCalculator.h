f495ccc3ffd6f7033d274f6b35bfe1aeda187ec0
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2014-04-14 15:37:07 +0300

    added template method getMinorForElement()

--- sources/ClassicDeterminantCalculator.h
+++ sources/ClassicDeterminantCalculator.h
@@ -15,6 +15,45 @@ public:
 private:
     int defineSignForElement(natural row_index, 
                              natural column_index);
+    template<typename ELEMENT_TYPE>
+    Matrix<ELEMENT_TYPE> getMinorForElement(Matrix<ELEMENT_TYPE>& matrix,
+                                             natural row_index, 
+                                             natural column_index)
+    {
+        if(isNull(matrix))
+        {
+            return Matrix<ELEMENT_TYPE>(0, 0);
+        }
+        Matrix<ELEMENT_TYPE> minor(matrix.getRowsNumber()       - 1, 
+                                   matrix.getColumnsNumber()    - 1);
+        for(natural matrix_row_index = 0,
+            minor_row_index = 0; 
+            matrix_row_index < matrix.getRowsNumber();
+            ++matrix_row_index)
+        {
+            if(matrix_row_index == row_index)
+            {
+                continue;
+            }
+            for(natural matrix_column_index = 0,
+                minor_column_index = 0;
+                matrix_column_index < matrix.getColumnsNumber();
+                ++matrix_column_index)
+            {
+                if(matrix_column_index == column_index)
+                {
+                    continue;
+                }
+                minor.at(minor_row_index,
+                         minor_column_index) = matrix.at(matrix_row_index,
+                                                         matrix_column_index);
+                ++minor_column_index;
+            }
+            ++minor_row_index;
+        }
+        
+        return minor;
+    }
 };
 #endif	/* CLASSICDETERMINANTCALCULATOR_H */
 
