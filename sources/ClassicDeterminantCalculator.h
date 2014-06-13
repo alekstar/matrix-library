cb5a57d8e9c32f6066899c4b8038fd1bc1938975
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2014-06-13 15:28:10 +0300

    moved method getMinorMatrixForElement() out of class ClassicDeterminantCalculator to MatrixAlgorithms.h/.cpp

--- sources/ClassicDeterminantCalculator.h
+++ sources/ClassicDeterminantCalculator.h
@@ -6,63 +6,9 @@
 
 class ClassicDeterminantCalculator
 {
-    FRIEND_TEST(GetMinor, Minor0x0FromMatrix3x3);
-    FRIEND_TEST(GetMinor, Minor1x1FromMatrix3x3);
-    FRIEND_TEST(GetMinor, Minor0x1FromMatrix3x3);
-    FRIEND_TEST(GetMinor, Minor0x0FromMatrix1x1);
-    FRIEND_TEST(GetMinor, Minor0x0FromMatrix0x0);
-    FRIEND_TEST(GetMinor, Minor1x1FromMatrix2x3);
 public:
 
     double calculate(const Matrix<double> &matrix)
         throw(MatrixIsNotSquare);
-private:
-    template<typename ELEMENT_TYPE>
-    Matrix<ELEMENT_TYPE> getMinorMatrixForElement(
-                                            const Matrix<ELEMENT_TYPE>& matrix,
-                                            natural row_index, 
-                                            natural column_index)
-    {
-        matrix.throwExceptionIfColumnIndexIsWrong(column_index);
-        matrix.throwExceptionIfRowIndexIsWrong(row_index);
-        if(!isSquare(matrix))
-        {
-            throw MatrixIsNotSquare();
-        }
-        if(isNull(matrix) || 
-           (matrix.getRowsNumber() == 1 && matrix.getColumnsNumber() == 1))
-        {
-            return Matrix<ELEMENT_TYPE>(0, 0);
-        }
-        Matrix<ELEMENT_TYPE> minor(matrix.getRowsNumber()       - 1, 
-                                   matrix.getColumnsNumber()    - 1);
-        for(natural matrix_row_index = 0,
-            minor_row_index = 0; 
-            matrix_row_index < matrix.getRowsNumber();
-            ++matrix_row_index)
-        {
-            if(matrix_row_index == row_index)
-            {
-                continue;
-            }
-            for(natural matrix_column_index = 0,
-                minor_column_index = 0;
-                matrix_column_index < matrix.getColumnsNumber();
-                ++matrix_column_index)
-            {
-                if(matrix_column_index == column_index)
-                {
-                    continue;
-                }
-                minor.at(minor_row_index,
-                         minor_column_index) = matrix.at(matrix_row_index,
-                                                         matrix_column_index);
-                ++minor_column_index;
-            }
-            ++minor_row_index;
-        }
-        
-        return minor;
-    }
 };
 #endif	/* CLASSICDETERMINANTCALCULATOR_H */
--- tests/TestMatrixAlgorithms.cpp
+++ tests/TestMatrixAlgorithms.cpp
@@ -344,3 +344,86 @@ TEST(DefineSignForElement, SeveralCases)
     EXPECT_EQ(1,  defineSignForElement(2, 2));
 }
 
+TEST(GetMinor, Minor0x0FromMatrix3x3)
+{
+    Matrix<int> matrix3x3(3, 3);
+    matrix3x3.at(0, 0) = 1;
+    matrix3x3.at(0, 1) = 2;
+    matrix3x3.at(0, 2) = 3;
+    matrix3x3.at(1, 0) = 4;
+    matrix3x3.at(1, 1) = 5;
+    matrix3x3.at(1, 2) = 6;
+    matrix3x3.at(2, 0) = 7;
+    matrix3x3.at(2, 1) = 8;
+    matrix3x3.at(2, 2) = 9;
+    
+    Matrix<int> minor_for_0x0 = getMinorMatrixForElement(matrix3x3, 0, 0);
+    
+    EXPECT_EQ(2, minor_for_0x0.getRowsNumber());
+    EXPECT_EQ(2, minor_for_0x0.getColumnsNumber());
+    EXPECT_EQ(5, minor_for_0x0.at(0, 0));
+    EXPECT_EQ(6, minor_for_0x0.at(0, 1));
+    EXPECT_EQ(8, minor_for_0x0.at(1, 0));
+    EXPECT_EQ(9, minor_for_0x0.at(1, 1));
+}
+
+TEST(GetMinor, Minor1x1FromMatrix3x3)
+{
+    Matrix<int> matrix3x3(3, 3);
+    matrix3x3.at(0, 0) = 1;
+    matrix3x3.at(0, 1) = 2;
+    matrix3x3.at(0, 2) = 3;
+    matrix3x3.at(1, 0) = 4;
+    matrix3x3.at(1, 1) = 5;
+    matrix3x3.at(1, 2) = 6;
+    matrix3x3.at(2, 0) = 7;
+    matrix3x3.at(2, 1) = 8;
+    matrix3x3.at(2, 2) = 9;
+    
+    Matrix<int> minor_for_1x1 = getMinorMatrixForElement(matrix3x3, 1, 1);
+    
+    EXPECT_EQ(1, minor_for_1x1.at(0, 0));
+    EXPECT_EQ(3, minor_for_1x1.at(0, 1));
+    EXPECT_EQ(7, minor_for_1x1.at(1, 0));
+    EXPECT_EQ(9, minor_for_1x1.at(1, 1));
+}
+
+TEST(GetMinor, Minor0x1FromMatrix3x3)
+{
+    Matrix<int> matrix3x3(3, 3);
+    matrix3x3.at(0, 0) = 1;
+    matrix3x3.at(0, 1) = 2;
+    matrix3x3.at(0, 2) = 3;
+    matrix3x3.at(1, 0) = 4;
+    matrix3x3.at(1, 1) = 5;
+    matrix3x3.at(1, 2) = 6;
+    matrix3x3.at(2, 0) = 7;
+    matrix3x3.at(2, 1) = 8;
+    matrix3x3.at(2, 2) = 9;
+    
+    Matrix<int> minor_for_0x1 = getMinorMatrixForElement(matrix3x3, 0, 1);
+    
+    EXPECT_EQ(4, minor_for_0x1.at(0, 0));
+    EXPECT_EQ(6, minor_for_0x1.at(0, 1));
+    EXPECT_EQ(7, minor_for_0x1.at(1, 0));
+    EXPECT_EQ(9, minor_for_0x1.at(1, 1));
+}
+
+TEST(GetMinor, Minor0x0FromMatrix1x1)
+{
+    Matrix<int> matrix1x1(1, 1, 12);
+    Matrix<int> minor_for_0x0 = getMinorMatrixForElement(matrix1x1, 0, 0);
+    EXPECT_TRUE(isNull(minor_for_0x0));
+}
+
+TEST(GetMinor, Minor0x0FromMatrix0x0)
+{
+    Matrix<int> matrix0x0;
+    EXPECT_THROW(getMinorMatrixForElement(matrix0x0, 0, 0), RangeError);
+}
+
+TEST(GetMinor, Minor1x1FromMatrix2x3)
+{
+    Matrix<int>matrix2x3(2, 3, 10);
+    EXPECT_THROW(getMinorMatrixForElement(matrix2x3, 1, 1), MatrixIsNotSquare);
+}
