c49d9460051264e9274fdb693b780d00d34c4668
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2014-06-17 14:11:08 +0300

    added parameter epsilon for function areAlmostEqual()

--- sources/AreAlmostEqual.cpp
+++ sources/AreAlmostEqual.cpp
@@ -2,7 +2,9 @@
 #include <limits>
 #include <cmath>
 
-bool areAlmostEqual(const double operand1, const double operand2)
+bool areAlmostEqual(const double operand1, 
+                    const double operand2, 
+                    const double epsilon)
 {
-    return fabs(operand1 - operand2) < std::numeric_limits<double>::epsilon();
+    return fabs(operand1 - operand2) < epsilon;
 }
--- sources/AreAlmostEqual.h
+++ sources/AreAlmostEqual.h
@@ -1,8 +1,11 @@
 #ifndef AREEQUAL_H
 #define	AREEQUAL_H
 
+const double EPSILON_FOR_DOUBLE_COMPARISON = 0.00001;
 
-bool areAlmostEqual(const double operand1, const double operand2);
+bool areAlmostEqual(const double operand1, 
+                    const double operand2, 
+                    const double epsilon = EPSILON_FOR_DOUBLE_COMPARISON);
 
 #endif	/* AREEQUAL_H */
 
